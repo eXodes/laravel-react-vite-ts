54cba56f175e2bce15c4ccf6732c9ff16ed89a89
Exo Des <hello@exodes.net>
2021-10-13 20:07:26 +0800

    feat: update content

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -6,7 +6,7 @@ import "../css/app.css"
 render(
     <StrictMode>
         <div className="bg-gray-50 min-h-screen w-full flex">
-            <h1 className="m-auto text-5xl font-bold">Hello from React with Vite and HMR on Runcloud!</h1>
+            <h1 className="m-auto text-5xl font-bold">Hello from React with Vite and HMR on Runcloud with webhooks!</h1>
         </div>
     </StrictMode>,
     document.getElementById("app")
